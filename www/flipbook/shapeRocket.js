f3b6d08e29e4f8ed3edeb18cefc0f1ab0930f4b1
Deniz Sokullu <dsokullu@andrew.cmu.edu>
2017-12-17 14:45:20 -0500

    Updated comments

--- www/flipbook/shapeRocket.js
+++ www/flipbook/shapeRocket.js
@@ -1,6 +1,5 @@
 // Constructor function
 function Rocket(dna,target) {
-  // Physics of rocket at current instance
   this.pos = createVector(width/4, height/2);
   this.vel = createVector();
   this.acc = createVector();
@@ -9,11 +8,8 @@ function Rocket(dna,target) {
   this.target = target;
   this.color = target.color;
   this.shape = target.shape;
-  // Checkes rocket has reached target
   this.completed = false;
-  // Checks if rocket had crashed
   this.crashed = false;
-  // Gives a rocket dna
   if (dna) {
     this.dna = dna;
   } else {
@@ -25,18 +21,15 @@ function Rocket(dna,target) {
   this.applyForce = function(force) {
     this.acc.add(force);
   }
-  // Calulates fitness of rocket
   this.calcFitness = function() {
     // Takes distance to target
     var d = dist(this.pos.x, this.pos.y, target.pos.x, target.pos.y);
     // Maps range of fitness
     maxDist = Math.sqrt((width**2)+(height**2));
     this.fitness = map(d, 0, maxDist, maxDist, 0);
-    // If rocket gets to target increase fitness of rocket
     if (this.completed) {
       this.fitness *= 200;
     }
-    // If rocket does not get to target decrease fitness
     if (this.crashed) {
       this.fitness /= 10;
     }
@@ -47,11 +40,9 @@ function Rocket(dna,target) {
     // Maps range of fitness
     maxDist = Math.sqrt((width**2)+(height**2));
     fitness = map(d, 0, maxDist, maxDist, 0);
-    // If rocket gets to target increase fitness of rocket
-    // If rocket does not get to target decrease fitness
     return fitness;
   }
-  // Updates state of rocket
+  // Updates state
   this.update = function() {
     // Checks distance from rocket to target
     var d = dist(this.pos.x, this.pos.y, this.target.pos.x, this.target.pos.y);
@@ -63,23 +54,7 @@ function Rocket(dna,target) {
     if(this.pos.y < 0 || this.pos.x < 0 || this.pos.y > height || this.pos.x > width/2){
       this.crashed = true;
     }
-    // // Rocket hit the barrier
-    // if (this.pos.x > rx && this.pos.x < rx + rw && this.pos.y > ry && this.pos.y < ry + rh) {
-    //   this.crashed = true;
-    // }
-    // // Rocket has hit left or right of window
-    // if (this.pos.x > width || this.pos.x < 0) {
-    //   this.crashed = true;
-    // }
-    // // Rocket has hit top or bottom of window
-    // if (this.pos.y > height || this.pos.y < 0) {
-    //   this.crashed = true;
-    // }
-
-
-    //applies the random vectors defined in dna to consecutive frames of rocket
     this.applyForce(this.dna.genes[count]);
-    // if rocket has not got to goal and not crashed then update physics engine
     if (!this.completed && !this.crashed) {
       this.vel.add(this.acc);
       this.pos.add(this.vel);
@@ -87,7 +62,6 @@ function Rocket(dna,target) {
       this.vel.limit(4);
     }
   }
-  // displays rocket to window
   this.show = target.draw;
 
 }
